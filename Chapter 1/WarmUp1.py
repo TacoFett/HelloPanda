2af8f0c2f2b560e2e4bbccd135d3c310bbf5e447
Nathaniel Lehman <nathaniel.lehman.v1.0@gmail.com>
2026-01-26 23:17:49 -0500

    Set up initial Scene

---
+++ Chapter 1/WarmUp1.py
@@ -0,0 +1,22 @@
+from direct.showbase.ShowBase import ShowBase
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+app = MyApp()
+app.run()
